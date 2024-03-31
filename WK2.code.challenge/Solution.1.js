ba2e569ec6d2dfad651fa295155ec8ebbdd7845c
Hillary-Ndubi <hillarynyabaige@gmail.com>
2024-03-31 16:16:38 +0300

    Complete

---
+++ WK2.code.challenge/Solution.1.js
@@ -0,0 +1,14 @@
+//swapping case function
+function swapCase(string) {
+    return string.split('').map(char => {
+        if (char === char.toUpperCase()) {
+            return char.toLowerCase();
+        } else {
+            return char.toUpperCase();
+        }
+    }).join('');
+}
+
+const inputString = 'The Quick Brown Fox';
+const outputString = swapCase(inputString);
+console.log(outputString); // Output: 'tHE qUICK bROWN fOX'
